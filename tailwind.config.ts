de9dba36dfd9d1ef0ae873a12bace03a47611895
jaehee <ljh34210329@gmail.com>
2023-09-30 23:17:55 +0900

    Set typescript path alias

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 import defaultTheme from "tailwindcss/defaultTheme";
 
 let config: Config = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
             fontFamily: {
