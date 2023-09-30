259fdd4320f70d7fd8bd24540fa4293a79dcbc4b
jaehee <ljh34210329@gmail.com>
2023-09-30 22:49:14 +0900

    이미지 썸네일,og 이미지를 지정할 수 있도록 수정

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,17 @@ import {z, defineCollection} from "astro:content";
 
 const postCollection = defineCollection({
     type: "content",
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string().optional(),
         date: z.date(),
         editDate: z.date().optional(),
         series: z.string().optional(),
         tags: z.array(z.string()).optional(),
+        og: z.object({
+            img: image(),
+            alt: z.string(),
+        }).optional()
     }),
 });
 
