d523b0cb01f0241e9acbc82791b4e8d9489e59f3
jaehee <ljh34210329@gmail.com>
2023-10-03 21:41:47 +0900

    Add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: import.meta.env.PROD ? "https://jaehee.dev" : undefined,
   build: {
-      assets: "asset_dir"
+    assets: "asset_dir"
   }
 });
