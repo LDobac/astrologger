ac003ee1491800c0d3270b57744f196324e0b3e7
jaehee <ljh34210329@gmail.com>
2023-10-04 16:36:33 +0900

    Modify rss description

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,8 +3,6 @@ import GeneratePostDesc from "@utils/GeneratePostDesc";
 import {GetPosts} from "@utils/GetPosts";
 import type {APIRoute} from "astro";
 import {slug as slugger} from "github-slugger";
-import {marked} from "marked";
-import sanitizeHtml from "sanitize-html";
 
 export const GET: APIRoute = async (ctx) => {
     const site = ctx.site ?? (import.meta.env.SITE ?? "https://jaehee.dev");
@@ -13,7 +11,7 @@ export const GET: APIRoute = async (ctx) => {
     
     return rss({
         title: "Jaehee.dev",
-        description: "Test RSS",
+        description: "Recent posts in Jaehee.dev",
         site: site,
         items: posts.map((post) => ({
             title: post.data.title,
