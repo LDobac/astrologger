7d4db8b7289308a40c7da4c05af998fd24e0ff85
jaehee <ljh34210329@gmail.com>
2023-10-06 21:13:12 +0900

    Add katex expression in markdown
    - Add remark-math, rehype-katex package

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        syntaxHighlight: "shiki",
+        remarkPlugins: ["remark-math"],
+        rehypePlugins: ["rehype-katex"],
+        gfm: true,
+    },
   integrations: [tailwind(), sitemap()],
   site: import.meta.env.PROD ? "https://jaehee.dev" : undefined,
   build: {
